9d1f799f6051b0ee509bf7643c5cd202e2bbc38a
Blssel <1109047248@qq.com>
2018-03-16 09:45:34 +0800

    94. Binary Tree Inorder Traversal

--- LeetCode/94.py
+++ LeetCode/94.py
@@ -3,33 +3,32 @@
 __author__ = 'Zhiyu Yin'
 
 # Definition for a binary tree node.
-class TreeNode(object):
-  def __init__(self, x):
-    self.val = x
-    self.left = None
-    self.right = None
+# class TreeNode(object):
+#   def __init__(self, x):
+#     self.val = x
+#     self.left = None
+#     self.right = None
 
 class Solution(object):
-  ans_tmp=[]
   def inorderTraversal(self,root):
     """
       :type root: TreeNode
       :rtype: List[int]
     """
+    ans=[]
+    self.recursive_traversal(root,ans)
+    return ans
+
+  def recusive_traversal(root,ans)
     if root is None:
-      ans=Solution.ans_tmp
-      Solution.ans_tmp=[]
       return ans
     else:
       if root.left is not None:
-        self.inorderTraversal(root.left)
+	recursive_traversal(root.left)
       if root is not None:
-        Solution.ans_tmp.append(root.val)
+        ans.append(root.val)
       if root.right is not None:
-        self.inorderTraversal(root.right)
-      ans= Solution.ans_tmp
-      Solution.ans_tmp=[]
-      return ans
+        recursive_traversal(root.right)
        
 if __name__=='__main__':
   root=TreeNode(5)
