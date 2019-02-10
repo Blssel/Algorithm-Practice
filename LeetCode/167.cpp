6bee126232171aa51ac4940ebf25702a4427513b
Blssel <1109047248@qq.com>
2019-02-10 18:08:57 +0800

    167. Two Sum II - Input array is sorted

--- LeetCode/167.cpp
+++ LeetCode/167.cpp
@@ -18,3 +18,26 @@ public:
     }
     }
 };
+
+
+// 二分法 time limit exceeded
+/*class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res;
+        for(int i=0;i<numbers.size();i++){
+          int l=i+1,r=numbers.size(),mid;
+        while(l<=r){
+        mid = (l+r)/2;
+                if(numbers[i]+numbers[mid] == target){
+                    res.push_back(i+1);
+                    res.push_back(mid+1);
+                    return res;
+                }else{
+                    l=mid-1;
+                }
+            }
+        }
+        return res;
+    }
+};*/
