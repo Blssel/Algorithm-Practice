a2922d71c51eff8fd56f156b64fabed6cd33e874
Blssel <1109047248@qq.com>
2019-05-05 11:51:09 +0800

    复杂度稍微低了些，O(n!)

--- LeetCode/46.cpp
+++ LeetCode/46.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-  void helper(vector<int>& nums, vector<bool>& used, vector<int>& perm, int ele_left, vector<vector<int>>& res) {
-    cout << ele_left << endl;
-    if (ele_left == 0) {
-      //cout << "###########" << endl;
-      res.push_back(perm);
+  void swap(vector<int>& nums, int x, int y) {
+    int tmp = nums[x];
+    nums[x] = nums[y];
+    nums[y] = tmp;
+  }
+  void helper(vector<int>& nums, int p, vector<vector<int>>& res) {
+    if (p == nums.size()) {
+      res.push_back(nums);
       return;
     }
-    for (int i = 0; i < nums.size(); i++) {
-      if (!used[i]) {
-        used[i] = true;
-        perm.push_back(nums[i]);
-        helper(nums, used, perm, ele_left - 1, res);
-        //cout << "!!!!!!!" << endl;
-        //cout << nums.size() - 1 << endl;
-        perm.erase(perm.begin() + perm.size() - 1); //回溯
-        //cout << "*******" << endl;
-        used[i] = false;
-      }
+    for (int i = p; i < nums.size(); i++) {
+      swap(nums, p, i);
+      helper(nums, p + 1, res);
+      swap(nums, p, i);
     }
-
   }
   vector<vector<int>> permute(vector<int>& nums) {
     vector<vector<int>> res;
-    vector<bool> used(nums.size(), false);
-    vector<int> perm;
-    helper(nums, used, perm, nums.size(), res);
+    helper(nums, 0, res);
     return res;
   }
 };
