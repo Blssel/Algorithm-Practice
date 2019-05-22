c8c9452f9fdf1fc67777753c675dc415e9e3934c
Blssel <1109047248@qq.com>
2019-05-22 22:06:24 +0800

    效率不错，

--- LeetCode/171.cpp
+++ LeetCode/171.cpp
@@ -9,3 +9,19 @@ public:
     return res;
   }
 };
+
+
+class Solution {
+public:
+  int titleToNumber(string s) {
+    int len = s.length();
+    if (len == 0) return 0;
+    int res = s[len-1] - 'A' + 1;
+    int base = 1;
+    for (int i = len - 2; i >= 0; i--) {
+            base = base*26;
+      res += (s[i] - 'A' + 1) *base;
+    }
+    return res;
+  }
+};
