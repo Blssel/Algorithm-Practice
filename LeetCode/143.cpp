d27ec930a2fd1a5bb16d4f9215ae8e0348576dce
Blssel <1109047248@qq.com>
2019-06-21 16:29:23 +0800

    改进，还是太菜

--- LeetCode/143.cpp
+++ LeetCode/143.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
   void reorderList(ListNode* head) {
-    vector<ListNode*> pointers;
-    ListNode* p = head;
-    while (p != NULL) {
-      pointers.push_back(p);
-      p = p->next;
-    }
-    if (pointers.size() <= 2) {
+    if (head == NULL || head->next == NULL) {
       return;
     }
-    ListNode *cur, *next, *last, *pre;
-    for (int i = 0; i < (pointers.size() - 1) / 2; i++) {
-      cur = pointers[i];
-      next = cur->next;
-      last = pointers[pointers.size() - 1 - i];
-      pre = pointers[pointers.size() - 2 - i];
-
-      cur->next = last;
-      last->next = next;
-      pre->next = NULL;
+    ListNode *fast = head, *slow = head;  //快慢指针，快的走两步，慢的走一步，最终慢的指向中间
+    while (fast->next&&fast->next->next) {
+      fast = fast->next->next;
+      slow = slow->next;
+    }
+    ListNode *first = head, *second = slow->next;
+    slow->next = NULL;  //分为两个链表 first和second
+    
+    //反转第二个链表
+    ListNode *cur=second, *pre=NULL, *next=cur->next;
+    while (cur) {
+      cur->next = pre;
+      if (next == NULL) {
+        second = cur;
+        break;
+      }
+      pre = cur;
+      cur = next;
+      next = next->next;
+    }
+    //逐个插入
+    head = first;
+    ListNode *tmp;
+    while (second != NULL) {
+      tmp = first->next;
+      first->next = second;
+      second = second->next;
+      first->next->next = tmp;
+      first = first->next->next;
     }
   }
 };
+
